a5c66b39ac7138fbbdb9c48f1ef4913bde9b1286
Hanitacm <anitacm82@gmail.com>
2026-02-28 18:38:34 +0100

    add image cache

--- app/src/main/java/com/hanitacm/stackoverflowusers/ImageLoader.kt
+++ app/src/main/java/com/hanitacm/stackoverflowusers/ImageLoader.kt
@@ -2,6 +2,7 @@ package com.hanitacm.stackoverflowusers
 
 import android.graphics.BitmapFactory
 import android.util.Log
+import android.util.LruCache
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import kotlinx.coroutines.Dispatchers
@@ -10,22 +11,25 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 object ImageLoader {
-
+    private val cache = LruCache<String, ImageBitmap>((Runtime.getRuntime().maxMemory() / 1024 / 1024).toInt()/ 8)
     suspend fun getImageBitmap(url: String): ImageBitmap? {
-        return withContext(Dispatchers.IO) {
-            try {
-                val client = OkHttpClient()
-                val request = Request.Builder().url(url).build()
-                val response = client.newCall(request).execute()
-                response.body.byteStream().use { inputStream ->
-                    inputStream.let {
-                        BitmapFactory.decodeStream(it)?.asImageBitmap()
+        return cache[url]
+            ?: withContext(Dispatchers.IO) {
+                try {
+                    val client = OkHttpClient()
+                    val request = Request.Builder().url(url).build()
+                    val response = client.newCall(request).execute()
+                    response.body.byteStream().use { inputStream ->
+                        inputStream.let {
+                            val bitmap = BitmapFactory.decodeStream(it)?.asImageBitmap()
+                            cache.put(url, bitmap)
+                            return@withContext bitmap
+                        }
                     }
+                } catch (e: Exception) {
+                    Log.e("ImageLoader", "Error loading image: $url", e)
+                    null
                 }
-            } catch (e: Exception) {
-                Log.e("ImageLoader", "Error loading image: $url", e)
-                null
             }
-        }
     }
 }
