14ad7397744f1ebd3b23cda57e071211e4a25f83
Hanitacm <anitacm82@gmail.com>
2026-03-16 21:47:17 +0100

    add refresh

--- app/src/main/java/com/hanitacm/stackoverflowusers/ui/UserListScreen.kt
+++ app/src/main/java/com/hanitacm/stackoverflowusers/ui/UserListScreen.kt
@@ -1,5 +1,7 @@
 package com.hanitacm.stackoverflowusers.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -7,15 +9,17 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.hanitacm.stackoverflowusers.R
 import com.hanitacm.stackoverflowusers.ui.composables.ErrorMessage
-import com.hanitacm.stackoverflowusers.ui.composables.ProgressBar
 import com.hanitacm.stackoverflowusers.ui.composables.UserList
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,6 +27,7 @@ import com.hanitacm.stackoverflowusers.ui.composables.UserList
 internal fun UserListScreen(viewModel: UserListViewModel) {
     val uiState: UserListUiState by viewModel.viewState.collectAsStateWithLifecycle()
     val snackbarHostState = remember { SnackbarHostState() }
+    val pullRefreshState = rememberPullToRefreshState()
 
 
     Scaffold(
@@ -36,19 +41,35 @@ internal fun UserListScreen(viewModel: UserListViewModel) {
         },
     ) { paddingValues ->
         when (uiState) {
-            is UserListUiState.Loading -> ProgressBar(modifier = Modifier.padding(paddingValues))
-            is UserListUiState.Success -> UserList(
-                users = (uiState as UserListUiState.Success).users,
-                onFollowClick={ userId->
-                    viewModel.followUser(userId)
-                },
-                onUnfollowClick={userId->
-                    viewModel.unFollowUser(userId)
-                },
-                modifier = Modifier.padding(paddingValues),
-            )
+            is UserListUiState.Loading,   //-> ProgressBar(modifier = Modifier.padding(paddingValues))
+            is UserListUiState.Success -> Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+            ) {
+                PullToRefreshBox(
+                    isRefreshing = uiState is UserListUiState.Loading,
+                    state = pullRefreshState,
+                    modifier = Modifier.align(Alignment.TopCenter),
+                    onRefresh = viewModel::onRefresh
+                ) {
+                    if (uiState is UserListUiState.Success){
+                    UserList(
+                        users = (uiState as UserListUiState.Success).users,
+                        onFollowClick = { userId ->
+                            viewModel.followUser(userId)
+                        },
+                        onUnfollowClick = { userId ->
+                            viewModel.unFollowUser(userId)
+                        },
+                    )}
+                }
+            }
 
-            is UserListUiState.Error -> ErrorMessage(stringResource(id = R.string.error_message_text))
+
+            is UserListUiState.Error -> ErrorMessage(
+                stringResource(id = R.string.error_message_text),
+                retryOnClick = { viewModel.onRefresh() })
 
 
         }
--- app/src/main/java/com/hanitacm/stackoverflowusers/ui/UserListViewModel.kt
+++ app/src/main/java/com/hanitacm/stackoverflowusers/ui/UserListViewModel.kt
@@ -4,22 +4,34 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hanitacm.stackoverflowusers.data.UsersRepository
 import com.hanitacm.stackoverflowusers.ui.model.asUiModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class UserListViewModel(private val repository: UsersRepository) : ViewModel() {
-    val viewState: StateFlow<UserListUiState> =
-        combine(repository.users, repository.followees) { users, followees ->
-            val userUiList = users.map { user ->
-                user.asUiModel(isFollowee = followees.contains(user.id))
+    private val refreshTrigger = MutableSharedFlow<Unit>()
+
+    val viewState: StateFlow<UserListUiState> = refreshTrigger
+        .onStart { emit(Unit) }
+        .flatMapLatest {
+            combine(repository.users, repository.followees) { users, followees ->
+                val userUiList = users.map { user ->
+                    user.asUiModel(isFollowee = followees.contains(user.id))
+                }
+                UserListUiState.Success(userUiList) as UserListUiState
             }
-            UserListUiState.Success(userUiList)
-        }.catch<UserListUiState> { emit(UserListUiState.Error) }
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), UserListUiState.Loading)
+            .onStart { emit(UserListUiState.Loading) }
+            .catch { emit(UserListUiState.Error) }
+        }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), UserListUiState.Loading)
 
     fun followUser(userId: Int) {
         viewModelScope.launch {
@@ -32,9 +44,10 @@ class UserListViewModel(private val repository: UsersRepository) : ViewModel() {
             repository.unfollowUser(userId)
         }
     }
+
+    fun onRefresh() {
+        viewModelScope.launch {
+            refreshTrigger.emit(Unit)
+        }
+    }
 }
-
-
-
-
-
